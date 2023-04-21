177bc5b38f9d6caf6e4a558a24da3573846227bc
/jomar <51296266+jomar0@users.noreply.github.com>
2023-04-21 02:34:19 +0100

    prog bar to bicubic

--- bicubic.py
+++ bicubic.py
@@ -1,4 +1,5 @@
 import torch
+from tqdm import tqdm
 from data import ImagePairs
 from utils import create_dataloaders, psnr, ssim
 training_dataset = ImagePairs("/home/u1909943/MSc/ImagePairs/training")
@@ -7,14 +8,16 @@ evaluation_dataset = ImagePairs("/home/u1909943/MSc/ImagePairs/evaluation")
 _, dataloader = create_dataloaders(training_dataset, evaluation_dataset, num_workers=0, batch_size=16)
 avg_psnr = 0.0
 avg_ssim = 0.0
-for data in dataloader:
-    input, gt = data
-    input.to(device)
-    gt.to(device)
+with tqdm(total=len(dataloader.dataset)) as t:
+    for data in dataloader:
+        input, gt = data
+        input.to(device)
+        gt.to(device)
 
-    output = torch.nn.functional.interpolate(input, scale_factor=2, mode='bicubic')
-    avg_psnr += psnr(output, gt)
-    avg_ssim += ssim(output, gt)
+        output = torch.nn.functional.interpolate(input, scale_factor=2, mode='bicubic')
+        avg_psnr += psnr(output, gt)
+        avg_ssim += ssim(output, gt)
+        t.update(1)
 avg_psnr = avg_psnr / len(dataloader.dataset)
 avg_ssim = avg_ssim / len(dataloader.dataset)
 
