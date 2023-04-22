0f01fe884d558db79f683ee942f613826e53387c
/jomar <51296266+jomar0@users.noreply.github.com>
2023-04-22 14:54:28 +0100

    tupleify kernels

--- nonshrinking_based.py
+++ nonshrinking_based.py
@@ -14,7 +14,7 @@ class ResBlockNet(nn.Module):
         self.input_layer = ConvBlock(
             in_channels=config["input_layer"]["in_channels"],
             out_channels=config["input_layer"]["out_channels"],
-            kernel_size=config["input_layer"]["kernel"],
+            kernel_size=tuple(config["input_layer"]["kernel"]),
             type="conv",
         )
         self.map_blocks = nn.ModuleList()
@@ -24,13 +24,13 @@ class ResBlockNet(nn.Module):
                     ConvBlock(
                         in_channels=config["mapping_blocks"][i][0]["in_channels"],
                         out_channels=config["mapping_blocks"][i][0]["out_channels"],
-                        kernel_size=config["mapping_blocks"][i][0]["kernel"],
+                        kernel_size=tuple(config["mapping_blocks"][i][0]["kernel"]),
                         type="dws",
                     ),
                     ConvBlock(
                         in_channels=config["mapping_blocks"][i][1]["in_channels"],
                         out_channels=config["mapping_blocks"][i][1]["out_channels"],
-                        kernel_size=config["mapping_blocks"][i][0]["kernel"],
+                        kernel_size=tuple(config["mapping_blocks"][i][0]["kernel"]),
                         type="dws",
                     ),
                 )
@@ -40,13 +40,13 @@ class ResBlockNet(nn.Module):
             self.hidden_layers.append(ConvBlock(
                 in_channels=config["hidden_layers"][i]["in_channels"],
                 out_channels = config["hidden_layers"][i]["out_channels"],
-                kernel_size=config["hidden_layers"][i]["kernel"],
+                kernel_size=tuple(config["hidden_layers"][i]["kernel"]),
                 type="dws"
             ))
         self.output_layer = qnn.QuantConvTranspose2d(
             in_channels=config["output_layer"]["in_channels"],
             out_channels=config["output_layer"]["out_channels"],
-            kernel_size=config["output_layer"]["kernel"],
+            kernel_size=tuple(config["output_layer"]["kernel"]),
             padding=config["output_layer"]["kernel"][0] // 2,
             stride=2,
             output_padding=1,
